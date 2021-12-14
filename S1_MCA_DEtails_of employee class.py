dc555e0f326506606d6c6ef0b760107bad66f6a4
Don <dongeevarghese44@gmail.com>
2021-12-14 15:12:58 +0530

    Added new details of n employee and print if 50000

---
+++ S1_MCA_DEtails_of employee class.py
@@ -0,0 +1,19 @@
+class Emplyee: 
+    def __init__(self, name,emid,salary,dept): 
+        self.name = name 
+        self.empid = emid
+        self.salary=salary
+        self.dept=dept
+list = [] 
+n=int(input("ENter Number of employes"));
+for i in range(n):
+ print("Enter details of ",i,"th Employee")
+ list.append( Emplyee(input("ENter name "), input("ENter Eid"),int(input("Enter salary")),input("Enter department")) )
+i=0;
+print("\n\n\nDEtails of employees annual salary >50k\n\n");
+for obj in list:
+    if(int(obj.salary)==50000):
+        i=i+1
+        print("details of ",i,"employee\n");
+        print( "name=",obj.name,"\nEmployee id:" ,obj.empid,"\n SALARY:",obj.salary,"\n DEPARTMENT:",obj.dept)
+        print("\n\n\n");
--- S1_MCA_n_students using class.py
+++ S1_MCA_n_students using class.py
@@ -9,7 +9,9 @@ for i in range(n):
  print("Enter details of ",i,"th students")
  list.append( Student(input("ENter name "), input("ENter roll"),input("Enter course")) )
 i=0;
+print("\n\n\nDEtails of sudents\n\n");
 for obj in list:
     i=i+1
-    print("details of ",i,"Students");
+    print("details of ",i,"Students\n");
     print( "name=",obj.name,"\nRoll no:" ,obj.roll,"\n Course:",obj.curs)
+    print("\n\n\n");
